69e27e52c1863f6a1f73ab644cf7de1f2e0743b8
duyhungphung <phungduyhung1992@gmail.com>
2024-02-19 11:25:06 +0700

    First commit

--- src/index.js
+++ src/index.js
@@ -3,12 +3,43 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import storageSession from "redux-persist/lib/storage/session";
+import {persistReducer, persistStore} from "redux-persist";
+import {PersistGate} from "redux-persist/integration/react";
+import {combineReducers, createStore} from "redux";
+import authReducer from "./reducers/authReducer";
+import {Provider} from "react-redux";
+import {BrowserRouter} from "react-router-dom";
+
+const persistConfig = {
+    key: 'root',
+    storage: storageSession
+}
+
+const rootReducer = combineReducers({
+    auth: authReducer,
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = createStore(
+    persistedReducer,
+);
+
+const persistor = persistStore(store);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+            <PersistGate loading = {null} persistor = {persistor}>
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>
+            </PersistGate>
+        </Provider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/pages/Admin.js
@@ -0,0 +1,11 @@
+
+
+const Admin = () => {
+    return(
+        <div>
+            <h1>Admin page</h1>
+        </div>
+    )
+}
+
+export default Admin;
